83303c7c9fe5ce01ba48aa1c96f844e4a910a769
Nathan Long <him@nathanmlong.com>
2016-06-01 12:17:33 -0400

    Implement 'without' efficiently, to be reused by 'sub-alphagram?'

--- lib/anagrams.ex
+++ lib/anagrams.ex
@@ -140,10 +140,42 @@ defmodule Anagrams do
 
   end
 
-  # Takes two alphagrams, subtracts one from the other
-  # TODO - implement more efficiently since we know that these are sorted?
-  def without(haystack, needles) do
-    haystack -- needles
+  def without(outer, inner) do
+    case without(outer, inner, []) do
+      {:ok, acc} ->
+        acc |> :lists.reverse
+      {:error, message} ->
+        {:error, message}
+    end
+  end
+
+  # We've looked through everything and may have results
+  def without([] = _outer, [] = _inner, acc) do
+    {:ok, acc}
+  end
+
+  # We've filtered out inner, so just grab the rest of the letters from outer
+  def without([h | t] = _outer, [] = _inner, acc) do
+    without(t, [], [h | acc])
+  end
+
+  def without([] = _outer, _inner, _acc) do
+    {:error, "some letters in inner are not in outer"}
+  end
+
+  # We've run past the point where we can find what we're looking for
+  def without([outer_h | _outer_t], [inner_h | _inner_t], _acc) when outer_h  > inner_h do
+    {:error, "some letters in inner are not in outer"}
+  end
+
+  # heads match - this is a letter we want to remove
+  def without([outer_h | outer_t], [inner_h | inner_t], acc)    when outer_h == inner_h do
+    without(outer_t, inner_t, acc)
+  end
+
+  # Keep this letter from outer and keep looking for others to filter out
+  def without([outer_h | outer_t], [inner_h | inner_t], acc)    when outer_h  < inner_h do
+    without(outer_t, [inner_h | inner_t], [outer_h | acc])
   end
 
 end
