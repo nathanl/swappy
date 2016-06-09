45660c04c453d00d61f2020061c5f143fcf7c60f
Nathan Long <him@nathanmlong.com>
2016-06-09 08:44:29 -0400

    Task to clean up english dictionaries & apply to common_words

---
+++ lib/mix/tasks/clean_up_english_dictionary.ex
@@ -0,0 +1,36 @@
+defmodule Mix.Tasks.CleanUpEnglishDictionary do
+  use Mix.Task
+
+  @shortdoc "Opinionated filter for English dictionaries"
+  @moduledoc """
+  Opinionated filter for English dictionaries - removes 'useless' short (1-2 character) words for performance.
+  Which short words are useful is a matter of opinion - eg, I decided that 'mb' was useful but 'co' wasn't.
+  In any case, anagrams are found by repeatedly subtracting valid words, so the more very short words we have, the more times we must recurse, and the longer it takes us. The difference can be dramatic, so it's definitely worth filtering your dictionary somehow, even if you don't use this exact filter.
+  """
+  # This is the opinionated part.
+  @valid_short_words ~w[a i o ab ad ah am an as at aw ax ay be by cc cd do dj eh ex go ha he hi ho id ie if in is it iv kb la lo ma mb mc md me mg mr ms my no of oh ok on or ow ox pa pc pi rd re so st tm to tv uh um up us vc vs we ye yo]
+
+  def run([input_file_path, output_file_path]) do
+      output = input_file_path 
+      |> Path.expand
+      |> File.stream!
+      |> Enum.map(&String.strip/1)
+      |> Enum.filter(&valid_word?/1)
+
+      # Add line breaks
+      output = output |> Enum.map(fn (result) -> [result, "\n"] end)
+
+      output_file_path
+      |> Path.expand
+      |> File.write!(output)
+    end
+
+    defp valid_word?(word) do
+      cond do
+        String.length(word) > 2    -> true
+        word in @valid_short_words -> true
+        true -> false
+      end
+    end
+
+end
