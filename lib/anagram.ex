5b77276ec239b07e92f2dd0e5d2b14c487bfd77c
Nathan Long <him@nathanmlong.com>
2016-06-09 17:23:48 -0400

    Define fewer things on user module

--- lib/anagram.ex
+++ lib/anagram.ex
@@ -35,38 +35,10 @@ defmodule Anagram do
       def of(phrase, wordlist) do
         dict          = Anagram.Dictionary.to_dictionary(wordlist, legal_codepoints)
         dict_entries  = Map.keys(dict) # TODO - make this ordered like input dict
-        anagrams = anagrams_for(Anagram.Alphagram.to_alphagram(phrase, legal_codepoints), dict_entries)
+        anagrams = Anagram.anagrams_for(Anagram.Alphagram.to_alphagram(phrase, legal_codepoints), dict_entries)
         anagrams |> Enum.map(&human_readable(&1, dict)) |> List.flatten
       end
 
-      defp anagrams_for([], _dict_entries) do
-        [[]]
-      end
-
-      # catbat
-      # set([["a", "b", "t"], ["a", "c", "t"]], ...)
-      # phrase is a alphagram; dict_entries is a set of alphagrams
-      # return a set of answers - each answer is a list of alphagrams,
-      # each answer contains exactly the letters of the input phrase
-      # dict_entries is an enumerable.
-      # returns a Set.
-      defp anagrams_for(phrase, dict_entries) do
-        usable_entries = usable_entries_for(dict_entries, phrase)
-
-        init_acc = %{dict: usable_entries |> Enum.map(&elem(&1, 1)), results: []}
-        %{results: results} = Enum.reduce(usable_entries, init_acc, fn({phrase_without_entry, entry}, acc) ->
-          anagrams_without_entry = anagrams_for(phrase_without_entry, acc.dict)
-
-          new_results = Enum.reduce(anagrams_without_entry, acc.results, fn (anagram_without_entry, results) ->
-            anagram_with_entry = [entry | anagram_without_entry]
-            [anagram_with_entry | results]
-          end)
-
-          %{dict: tl(acc.dict), results: new_results}
-        end)
-
-        results
-      end
 
       # Convert a list of alphagrams to a list of human-readable anagrams
       # e.g. [ alphagram("race"), alphagram("car") ] =>
@@ -86,16 +58,6 @@ defmodule Anagram do
         end)
       end
 
-      def usable_entries_for(dict_entries, phrase) do
-        dict_entries
-        |> Enum.reduce([], fn (entry, acc) ->
-          case Anagram.Alphagram.without(phrase, entry) do
-            {:ok, outer, inner} -> [ {outer, inner} | acc]
-            _ -> acc
-          end
-        end)
-      end
-
       def wordlists do
         Anagram.default_wordlists
       end
@@ -107,6 +69,45 @@ defmodule Anagram do
       defoverridable [wordlists: 0, legal_codepoints: 0]
     end
 
+  end
+
+  def anagrams_for([], _dict_entries) do
+    [[]]
+  end
+
+  # catbat
+  # set([["a", "b", "t"], ["a", "c", "t"]], ...)
+  # phrase is a alphagram; dict_entries is a set of alphagrams
+  # return a set of answers - each answer is a list of alphagrams,
+  # each answer contains exactly the letters of the input phrase
+  # dict_entries is an enumerable.
+  # returns a Set.
+  def anagrams_for(phrase, dict_entries) do
+    usable_entries = usable_entries_for(dict_entries, phrase)
+
+    init_acc = %{dict: usable_entries |> Enum.map(&elem(&1, 1)), results: []}
+    %{results: results} = Enum.reduce(usable_entries, init_acc, fn({phrase_without_entry, entry}, acc) ->
+      anagrams_without_entry = Anagram.anagrams_for(phrase_without_entry, acc.dict)
+
+      new_results = Enum.reduce(anagrams_without_entry, acc.results, fn (anagram_without_entry, results) ->
+        anagram_with_entry = [entry | anagram_without_entry]
+        [anagram_with_entry | results]
+      end)
+
+      %{dict: tl(acc.dict), results: new_results}
+    end)
+
+    results
+  end
+
+  def usable_entries_for(dict_entries, phrase) do
+    dict_entries
+    |> Enum.reduce([], fn (entry, acc) ->
+      case Anagram.Alphagram.without(phrase, entry) do
+        {:ok, outer, inner} -> [ {outer, inner} | acc]
+        _ -> acc
+      end
+    end)
   end
 
 end
