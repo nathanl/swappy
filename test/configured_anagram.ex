e6e590732511c5c071437d7913531473a0f153a3
Nathan Long <him@nathanmlong.com>
2016-06-09 08:54:15 -0400

    Make ConfiguredAnagram as simple as possible

--- test/configured_anagram.ex
+++ test/configured_anagram.ex
@@ -1,14 +1,3 @@
 defmodule ConfiguredAnagram do
   use Anagram
-  @dictionaries Anagram.Dictionary.load_files(Application.get_env(:anagrams, :dictionary_files))
-  @legal_codepoints Application.get_env(:anagrams, :legal_codepoints)
-
-  def legal_codepoints do
-    @legal_codepoints
-  end
-
-  def dictionaries do
-    @dictionaries
-  end
-
 end
