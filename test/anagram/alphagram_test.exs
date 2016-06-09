bc2231e79ce385440ccdc9e55b9596d6fd4fbba9
Nathan Long <him@nathanmlong.com>
2016-06-09 12:58:40 -0400

    Break out separate examples in AlphagramTest

--- test/anagram/alphagram_test.exs
+++ test/anagram/alphagram_test.exs
@@ -2,12 +2,27 @@ defmodule Anagram.AlphagramTest do
   use ExUnit.Case
   doctest Anagram.Alphagram
 
-  test "without" do
+  test "subtracting nothing from a list" do
     assert Anagram.Alphagram.without(["a"                ], [])         == {:ok, ["a"], []}
+  end
+
+  test "subtracting everything from a one-item list" do
     assert Anagram.Alphagram.without(["a"                ], ["a"])      == {:ok, [], ["a"]}
-    assert Anagram.Alphagram.without(["a", "a"           ], ["a"])      == {:ok, ["a"], ["a"]}
+  end
+
+  test "subtracting everything from a multi-item list" do
     assert Anagram.Alphagram.without(["a", "a"           ], ["a", "a"]) == {:ok, [], ["a", "a"]}
-    assert Anagram.Alphagram.without(["a", "b", "c", "d" ], ["b", "c"]) == {:ok, ["a", "d"], ["b", "c"]}
+  end
+
+  test "subtracting the first item from a list" do
+    assert Anagram.Alphagram.without(["a", "a"           ], ["a"])      == {:ok, ["a"], ["a"]}
+  end
+
+  test "subtracting some items from different parts of a list" do
+    assert Anagram.Alphagram.without(["a", "b", "c", "d" ], ["b", "d"]) == {:ok, ["a", "c"], ["b", "d"]}
+  end
+
+  test "trying to subtract an item that's not in the list" do
     assert Anagram.Alphagram.without(
       ["a", "b"], ["x"]
     ) == {:error, "outer does not contain all letters of inner", {["a", "b"], ["x"]}}
