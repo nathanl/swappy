115c699f5387b473b5cadd334202df8ef088c013
Jay Hayes <ur@iamvery.com>
2016-06-17 13:35:11 -0500

    Reference built-in wordlist file

--- config/test.exs
+++ config/test.exs
@@ -1,4 +1,4 @@
 use Mix.Config
 
-config :anagrams, dictionary_files: %{ default: "~/code/anagram_wordlists/pruned_wordlist_by_length.txt", }
+config :anagrams, dictionary_files: %{ default: "lib/common_words_dictionary.txt", }
 config :anagrams, :legal_codepoints, 97..122 # lowercase a..z 
