a08242bd0a0d7f89f38b1eee03e7acdee0255ca2
Nathan Long <him@nathanmlong.com>
2016-06-21 16:52:57 -0400

    Use my hacked fork of PriorityQueue

--- test/max_priority_queue_test.exs
+++ test/max_priority_queue_test.exs
@@ -1,4 +1,4 @@
-defmodule SwappyTest do
+defmodule MaxPriorityQueueTest do
   use ExUnit.Case
 
   # I want leftmost to win - so increase numbers as we go
@@ -8,21 +8,21 @@ defmodule SwappyTest do
   # therefore I need a max heap implementation.
 
   # https://github.com/ewildgoose/elixir_priority_queue/issues/1
+  # https://github.com/nathanl/elixir_priority_queue/tree/max_hack
 
-
-  test "priority_queue" do
+  test "max heap priority queue" do
     q = PriorityQueue.new
-    q = PriorityQueue.put(q, {[0,1], "cake"})
-    q = PriorityQueue.put(q, {[0,1,1], "gold"})
-    q = PriorityQueue.put(q, {[0,2], "bread"})
-    q = PriorityQueue.put(q, {[0,3], "mush"})
-    q = PriorityQueue.put(q, {[0,4], "poop"})
+    q = PriorityQueue.put(q, {[0,-1,-1], "gold"})
+    q = PriorityQueue.put(q, {[0,-1], "cake"})
+    q = PriorityQueue.put(q, {[0,-2], "bread"})
+    q = PriorityQueue.put(q, {[0,-3], "mush"})
+    q = PriorityQueue.put(q, {[0,-4], "poop"})
     {{_p, a1}, q} = PriorityQueue.pop!(q)
     {{_p, a2}, q} = PriorityQueue.pop!(q)
     {{_p, a3}, q} = PriorityQueue.pop!(q)
     {{_p, a4}, q} = PriorityQueue.pop!(q)
-    {{_p, a5}, q} = PriorityQueue.pop!(q)
-    assert [a1, a2, a3, a4, a5] == ["cake", "bread", "mush", "poop"]
+    {{_p, a5}, _q} = PriorityQueue.pop!(q)
+    assert [a1, a2, a3, a4, a5] == ["gold", "cake", "bread", "mush", "poop"]
   end
 
 end
