b49dd7b2f88bd8c53647578835ae61d2ef91b58b
Nathan Long <him@nathanmlong.com>
2016-06-21 13:44:04 -0400

    Support limits in mix generate_anagrams & improve examples

--- lib/mix/tasks/generate.ex
+++ lib/mix/tasks/generate.ex
@@ -5,27 +5,47 @@ defmodule Mix.Tasks.GenerateAnagrams do
   @shortdoc "Simple task to generate anagrams for a phrase"
   @moduledoc """
   Uses the default dictionary. Usage:
-    mix generate_anagrams "my phrase"
+    mix generate_anagrams "hoverboard fever"
 
-  If you give a second argument, it will be taken as letters to omit. Eg:
-    mix generate_anagrams "my phrase" "as"
+  If you give a second argument, it will be taken as a limit for the number of anagrams to generate. Eg:
+    mix generate_anagrams "hoverboard fever" 10
 
-  This can be used to work toward something funny - eg, if you think the word "as" is funny, and you
-  see it in your results somewhere, exclude it and run again, knowing that whatever you find, you can
-  add "as" back to it. Repeat as necessary.
+  If the limit isn't a valid number, it will be treated as "infinity". Eg:
+    mix generate_anagrams "hoverboard fever" infinity
+
+  If you give a third argument, it will be treated as words the anagram must include. Eg:
+    mix generate_anagrams "hoverboard fever" 10 "rob drove"
+
+  This can be used to work toward something funny - if you see a word or words
+  you like, start requiring them, run again, and repeat.
   """
+
+  # TODO - detect closed pipe and stop gracefully? Eg if piping to head
   def run([phrase]) do
     anagrams_of(phrase) |> Enum.each(&(IO.puts(&1)))
   end
 
-  def run([phrase, without]) do
-    # TODO - detect closed pipe and stop gracefully? Eg if piping to head
+  def run([phrase, limit]) do
+    anagrams_of(phrase, %{limit: parse_int(limit)}) |> Enum.each(&(IO.puts(&1)))
+  end
+
+  def run([phrase, limit, without]) do
     alphagrams = [phrase, without] |> Enum.map(&Swappy.Alphagram.to_alphagram/1)
     case apply(Swappy.Alphagram, :without, alphagrams) do
       {:ok, remaining_alphagram, _without} ->
         remaining_phrase = Swappy.Alphagram.to_string(remaining_alphagram)
-        IO.puts ~s(phrase "#{remaining_phrase}" \("#{phrase}" without "#{without}"\) has anagrams:)
-        anagrams_of(remaining_phrase) |> Enum.each(&(IO.puts(&1)))
+        anagrams_of(remaining_phrase, %{limit: parse_int(limit)}) |> Enum.each(fn (anagram_without) ->
+          IO.puts [without, " ", anagram_without]
+        end)
     end
   end
+
+  defp parse_int(limit) when is_binary(limit) do
+    case Integer.parse(limit) do
+      :error -> :infinity
+      {val, _} -> val
+    end
+  end
+
+  defp parse_int(_limit), do: :infinity
 end
